08773fbdd633f03eccc0dafeefd41da0ef175545
Denis Rakitin <piggest@mail.ru>
2019-04-11 14:17:43 +0300

    init project

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Denis Rakitin on 2019-04-11.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Denis Rakitin on 2019-04-11.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
