89d9ede0b535a51e72a06fdce92e49ae21a23770
Denis Rakitin <piggest@mail.ru>
2019-04-12 22:36:32 +0300

    working with locationmanager

---
+++ Treads/Controller/LocationVC.swift
@@ -0,0 +1,29 @@
+//
+//  LocationVcViewController.swift
+//  Treads
+//
+//  Created by Denis Rakitin on 2019-04-12.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+
+    var manager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+
+}
